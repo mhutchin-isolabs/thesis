82a39673b55737b372c55035c39861171cffcaca
Michael Hutchinson <mhutchin@google.com>
2024-01-19 17:04:01 +0000

    week 1 iso updates

---
+++ clean_format_logs.sh
@@ -0,0 +1,6 @@
+rm *.bak*
+rm indent.log
+rm raw_papers/*.bak*
+rm raw_papers/indent.log
+rm chapters/*.bak*
+rm chapters/indent.log
---
+++ clean_latex_files.sh
@@ -0,0 +1,4 @@
+latexmk -C main.tex
+latexmk -C main_raw_papers.tex
+latexmk -C MJH_demo.tex
+latexmk -C test_doc.tex
