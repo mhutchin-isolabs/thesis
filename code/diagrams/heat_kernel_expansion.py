a185a503030a6e881cb49d9104ac67557b63ebf9
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2024-06-15 10:46:42 +0100

    updates

---
+++ code/diagrams/heat_kernel_expansion.py
@@ -0,0 +1,12 @@
+import jax
+import jax.numpy as jnp
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+import tikzplotlib
+
+base_path = "/home/mhutchin/GoogleDrive/University - Postgraduate/Theses/Final/"
+
+plt.style.use(base_path + "code/thesis.mplstyle")
